f7d8380cfdaa00f0dee19bb79eec2fc180e5fd1c
WanjiruKinyua <maureentally@gmail.com>
2023-08-28 05:49:32 -0700

    update

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,11 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        print()
         return True
-    except ValueError:
+    except Exception:
         return False
-
-
-print(safe_print_integer(42))
-print(safe_print_integer("hello"))
