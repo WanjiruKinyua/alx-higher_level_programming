e74c8cf89a23710d0984477249f35e2d938e3d74
WanjiruKinyua <maureentally@gmail.com>
2023-08-28 05:23:40 -0700

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,3 +10,8 @@ def safe_print_list(my_list=[], x=0):
     finally:
         print()
     return count
+
+
+my_list = [1, 2, 3, 4, 5]
+num_printed = safe_print_list(my_list, 3)
+print("Number of elements printed:", num_printed)
