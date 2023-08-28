8ec0c6ca138a5196618be96ca450d6ee7e6c675c
WanjiruKinyua <maureentally@gmail.com>
2023-08-28 15:00:22 -0700

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char_number in range(97, 123):
-    if char_number != 101 and char_number != 113:
-        print("{character}".format(character=chr(char_number)), end=""
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
