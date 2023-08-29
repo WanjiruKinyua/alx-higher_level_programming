5c654c66daa8d8895def0db7bdb1c31be9df8dfc
WanjiruKinyua <maureentally@gmail.com>
2023-08-29 14:48:00 -0700

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Wanjiru Kinyua
+"""
+This is a module that Write a class
+Square that defines a square by: (based on 0-square.py)
+"""
+
+
+class Square:
+    """class variable size"""
+    def __init__(self, size):
+        self.__size = size
+        """
+        initialize size
+        """
